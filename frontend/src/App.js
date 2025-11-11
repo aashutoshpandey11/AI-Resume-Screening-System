e235a9315a4e3b2e28e6b4f4ce42d6d4b72aa127
Aashutosh Pandey <aashutoshp.03@gmail.com>
2025-11-12 00:33:01 +0545

    Add backend and frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ResumeUpload from "./components/ResumeUpload";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AI Resume Screening System</h1>
+      <ResumeUpload />
+    </div>
+  );
+}
+
+export default App;
